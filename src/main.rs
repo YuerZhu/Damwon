7ca1ee5ad84ff2f889ddb9450eb0afcaecd4e267
mzayy <mzayy2@illinois.edu>
2021-11-18 15:16:02 -0600

    Clean

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 extern crate image;
 
-pub functions;
-
 pub mod functions;
+
 use image::{DynamicImage, GenericImageView, ImageBuffer, Pixel, RgbImage, RgbaImage, SubImage, imageops};
 
 fn main() {
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
+
+
+    /* Chunks tutorial
     let mut img =  image::open("rust.png").unwrap();    
 
     let (vert, horiz) = (3,3);
@@ -21,26 +23,28 @@ fn main() {
             vec[h][v].save( s).unwrap();
         }
     }
-    //
-    
+    */
+    /* Encrypt tutorial
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let mut img = image::open("bun.jpg").unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+    let pixel = img.get_pixel(300, 300);
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", pixel.0[0]);
+    img = encrypt(img);
+    img.save("encr.png").unwrap();
+    img = decrypt(img);
+    // img = encrypt(img);
+    // Write the contents of this image to the Writer in PNG format.
+    img.save("decr.png").unwrap();
+    */
 }
 
 
 // fn main() {
-//     // Use the open function to load an image from a Path.
-//     // `open` returns a `DynamicImage` on success.
-//     let mut img = image::open("bun.jpg").unwrap();
-
-//     // The dimensions method returns the images width and height.
-//     println!("dimensions {:?}", img.dimensions());
-//     let pixel = img.get_pixel(300, 300);
-//     // The color method returns the image's `ColorType`.
-//     println!("{:?}", pixel.0[0]);
-//     img = encrypt(img);
-//     img.save("encr.png").unwrap();
-//     img = decrypt(img);
-//     // img = encrypt(img);
-//     // Write the contents of this image to the Writer in PNG format.
-//     img.save("decr.png").unwrap();
+//     
 // }
 
